cdb3f9135f0cd84ad135f76f1dd4f71ae57bc4f2
Roman Shevchenko <rrrooommmaaa@mail.ru>
2020-03-20 20:25:43 +0300

    string::append fix

--- src/rnp/fficli.cpp
+++ src/rnp/fficli.cpp
@@ -733,7 +733,7 @@ cli_rnp_unescape_for_regcomp(const std::string &src)
         offset += matches[0].rm_eo;
     }
 
-    result.append(src, offset);
+    result.append(src.begin() + offset, src.end());
 
     return result;
 }
